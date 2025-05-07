5cfe7d1a8bf8f719eac5cd40805acf7b06e171da
Laís de Souza Neves <116578290+NevesLais@users.noreply.github.com>
2025-05-06 22:46:38 -0300

    atv.3.py

--- atv.3.py
+++ atv.3.py
@@ -6,14 +6,9 @@ b = 20
 c = -525
 
 
-delta = b**2 - 4*a*c
+baoQuadrado = b * b
 
+delta = a * c
 
-x1 = ( -b + delta ** (1/2) / 2 * a
-x2 = ( -b - delta ** (1/2) / 2 * a
-
-print(x1)
-print (x2)
-
-idade = max(raiz1, raiz2)
-print(f"Sua idade é: {idade}")
+print(str(delta))
+delta = -4 * delta
